ae5785bd9c5880e81f9b2e2e624cd1fdd5ed3ec1
Paul <pwgibbs10@gmail.com>
2022-05-29 01:41:49 -0600

    Created another variant of FahrenheitToCelsius

---
+++ fahrenheit-celsius/FahrenheitToCelsiusV2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/* Uses floats instead of integers for more precise arithmetic */
+
+int main() {
+	float fahr, celsius;
+	int lower, upper, step;
+
+	lower = 0;
+	upper = 300;
+	step = 20;
+
+	fahr = lower;
+
+	while (fahr <= upper) {
+		celsius = (5.0/9.0) * (fahr-32.0);
+		printf("%3.0f %6.1f\n", fahr, celsius);
+		fahr = fahr + step;
+	}
+
+	return 0;
+}
