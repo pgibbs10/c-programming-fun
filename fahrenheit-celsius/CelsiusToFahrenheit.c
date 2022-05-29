f3725c8a77f085f9457a2d130a48628236e8be97
Paul <pwgibbs10@gmail.com>
2022-05-29 01:59:50 -0600

    Completed exercises 1-3 and 1-4

--- fahrenheit-celsius/CelsiusToFahrenheit.c
+++ fahrenheit-celsius/CelsiusToFahrenheit.c
@@ -12,12 +12,12 @@ int main() {
 
 	fahr = lower;
 
-	printf("\nFahrenheit to Celsius cheat sheet\n");
+	printf("\nCelsius to Fahrenheit cheat sheet\n");
 
-	while (fahr <= upper) {
-		celsius = (5.0/9.0) * (fahr-32.0);
-		printf("%3.0f %6.1f\n", fahr, celsius);
-		fahr = fahr + step;
+	while (celsius <= upper) {
+		fahr = (celsius * (9.0/5.0)) + 32.0;
+		printf("%3.0f %6.0f\n", celsius, fahr);
+		celsius = celsius + step;
 	}
 
 	printf("\n");
